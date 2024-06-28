6e5c1a42417dcfc6dceb3c353588059efb5efc1b
danimohli <danimohlife@gmail.com>
2024-06-28 12:55:03 -0700

    instal

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
-# install puppet-lint -v 2.5.0
+# This Puppet manifest installs Flask version 2.1.0 using pip3
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => Package['python3-pip'],
 }
