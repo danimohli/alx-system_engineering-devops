1a291a37994d2d14271730209e2cb22185c61de3
danimohli <danimohlife@gmail.com>
2024-06-29 14:21:19 +0100

    pup

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,27 +1,12 @@
-# Define class for SSH client configuration
-class ssh_config {
-
-  # Ensure SSH client configuration file exists
-  file { '/etc/ssh/ssh_config':
-    ensure => file,
-  }
-
-  # Manage SSH client configuration options
-  file_line { 'ssh_private_key':
-    path    => '/etc/ssh/ssh_config',
-    line    => '    IdentityFile ~/.ssh/school',
-    ensure  => present,
-    require => File['/etc/ssh/ssh_config'],
-  }
-
-  file_line { 'ssh_no_password_authentication':
-    path    => '/etc/ssh/ssh_config',
-    line    => '    PasswordAuthentication no',
-    ensure  => present,
-    require => File['/etc/ssh/ssh_config'],
-  }
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-# Apply the ssh_config class
-include ssh_config
-
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
