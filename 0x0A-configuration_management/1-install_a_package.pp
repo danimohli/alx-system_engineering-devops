bdf6753f887dbd51b521536407d7ba594ef35cdb
danimohli <danimohlife@gmail.com>
2024-06-29 06:30:32 +0100

    compact

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-# Using Puppet, install flask
+# This Puppet manifest installs Flask version 2.1.0 and a compatible version of Werkzeug using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
 
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
+}
