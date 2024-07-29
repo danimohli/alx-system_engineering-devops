9de2c2e419aa61fb4e8747ce61a9a5fca0d237d1
danimohli <danimohlife@gmail.com>
2024-07-29 15:43:56 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -39,8 +39,7 @@ def main(employee_id):
     done_tasks = [task for task in todo_list if task.get('completed')]
     number_of_done_tasks = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with" +
-          f"tasks({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
