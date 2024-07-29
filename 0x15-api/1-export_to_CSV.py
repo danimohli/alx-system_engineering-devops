758236b4b5fe5e64749f46af4cc31019f8c677d6
danimohli <danimohlife@gmail.com>
2024-07-29 16:37:32 +0100

    export task-0 csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,52 +10,50 @@ import requests
 import sys
 
 
-def fetch_employee_name(employee_id):
+def fetch_employee_data(employee_id):
     """
-    Fetch the employee's name using the employee ID.
+    Fetch the employee's data using the employee ID.
     """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(url)
-    user = response.json()
-    return user.get('name')
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    dos_ = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    todos_response = requests.get(dos_)
+    todos_data = todos_response.json()
+
+    return user_data[0], todos_data
 
 
-def fetch_todo_list(employee_id):
-    """
-    Fetch the TODO list for the given employee ID.
-    """
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = requests.get(url)
-    return response.json()
-
-
-def export_to_csv(employee_id, username, todo_list):
+def export_to_csv(employee_id, username, todos):
     """
     Export the TODO list to a CSV file.
     """
     filename = f"{employee_id}.csv"
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for task in todo_list:
-            writer.writerow([employee_id, username, task.get('completed'), task.get('title')])
+        for task in todos:
+            writer.writerow([employee_id, username,
+                            task.get('completed'), task.get('title')])
 
 
 def main(employee_id):
     """
-    Main function to fetch and display the TODO list progress and export to CSV.
+    Main function to fetch and display the TODO list progress
+    and export to CSV.
     """
-    employee_name = fetch_employee_name(employee_id)
-    todo_list = fetch_todo_list(employee_id)
+    user_data, todos = fetch_employee_data(employee_id)
+    _name = user_data.get('username')
 
-    total_tasks = len(todo_list)
-    done_tasks = [task for task in todo_list if task.get('completed')]
-    number_of_done_tasks = len(done_tasks)
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get('completed')]
+    _done_ = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {_name} is done with tasks({_done_}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
-    export_to_csv(employee_id, employee_name, todo_list)
+    export_to_csv(employee_id, _name, todos)
 
 
 if __name__ == "__main__":
