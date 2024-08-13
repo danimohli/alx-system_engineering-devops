7a5d558868eb576b6449ee506425063bc7a959d4
danimohli <danimohlife@gmail.com>
2024-08-13 14:27:52 +0100

    reduse

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-An API Request to Reddit for possible
-Users queries
+An API Request to Reddit for possible Users queries
 """
 
 import requests
@@ -10,19 +9,12 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API to return the num of subscribers for
-    a given subreddit.
-    If invalid; returns 0.
+    a given subreddit. Returns 0 if invalid.
     """
-    headers = {
-        'User-Agent': 'my_reddit_app'
-    }
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'my_reddit_app'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
-    elif response.status_code == 302:
-        return 0
-    else:
-        return 0
+        return response.json().get('data', {}).get('subscribers', 0)
+    return 0
