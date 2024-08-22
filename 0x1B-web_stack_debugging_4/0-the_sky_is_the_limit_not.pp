d8a32273d4b0a08b03904da8e96169e22ffe46ee
danimohli <danimohlife@gmail.com>
2024-08-22 21:52:33 +0100

    file limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,5 +3,4 @@
 exec { 'modify_nginx_file_descriptor_limit':
   command => command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-  onlyif  => 'grep -q "s/15/10000/" /etc/nginx/nginx.conf',
 }
